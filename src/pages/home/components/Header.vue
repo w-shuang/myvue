986b3f57dc92661537248474965438586656e3e9
w-shuang <1761731525@qq.com>
2020-05-21 20:54:45 +0800

    header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="header">
-       <div class="header-left">返回</div>
-       <div class="header-input">输入城市/景点/游玩主题</div>
-       <div class="header-right">城市</div>
+       <div class="header-left">
+         <div class="iconfont back-icon">&#xe624;</div>
+       </div>
+       <div class="header-input">
+         <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题
+       </div>
+       <div class="header-right">城市
+         <span class="iconfont arrow-icon">&#xe6aa;</span>
+       </div>
     </div>
 </template>
 
@@ -13,16 +20,20 @@ export default{
 </script>
 
 <style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
     .header
         display: flex
         line-height: 43px
         height: 43px
-        background: #00bcd4
+        background: $bgColor
         color: #fff
         .header-left
             width: 32px
             float: left
             margin-right:10px
+            .back-icon
+                text-align: center
+                font-size: 20px
         .header-input
             flex: 1
             height: 31px
@@ -30,10 +41,14 @@ export default{
             background: #fff
             border-radius: 5px
             margin-top: 6px
+            padding-left: 10px
             color:#ccc
             line-height: 31px
         .header-right
             width: 62px
             float: right
             text-align: center;
+            .arrow-icon
+                margin-left: -2px
+                font-size:12px
 </style>

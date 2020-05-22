725830a2a0eac6915495daf44ed9aaef59ced6f4
w-shuang <1761731525@qq.com>
2020-05-22 19:32:34 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="icons">
+        <swiper>
+             <swiper-slide v-for="(page,index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.imgurl">
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                </div>
+             </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeIcons',
+    data (){
+        return {
+            iconList: [{
+                id: '0001',
+                imgurl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                desc: '经典门票'
+            },{
+                id: '0002',
+                imgurl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '一日游'
+            },{
+                id: '0003',
+                imgurl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '玩转周边'
+            },{
+                id: '0004',
+                imgurl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '华清宫'
+            },{
+                id: '0005',
+                imgurl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '长恨歌'
+            },{
+                id: '0006',
+                imgurl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '动植物园'
+            },{
+                id: '0007',
+                imgurl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '泡温泉'
+            },{
+                id: '0008',
+                imgurl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '兵马俑'
+            },{
+                id: '0009',
+                imgurl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '华山'
+            }
+            ]
+        }
+    },
+    computed: {
+        pages (){
+            const pages = []
+            this.iconList.forEach((item, index) => {
+                const page = Math.floor(index / 8)
+                if(!pages[page]){
+                    pages[page] = []
+                }
+                pages[page].push(item)
+            })
+            return pages
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    @import '~styles/mixins.styl'
+    .icons >>> .swiper-container 
+        height: 0
+        padding-bottom: 50%
+    .icon  
+        position:relative
+        overflow: hidden 
+        float: left
+        width: 25%
+        heught: 0 
+        padding-bottom: 25%
+        .icon-img
+            position: absolute
+            top: 0
+            left: 0
+            right: 0
+            bottom: 22px
+            box-sizing: border-box
+            padding: 5px
+            .icon-img-content
+                display: block
+                margin: 0 auto
+                height: 100%
+        .icon-desc
+            position: absolute
+            left: 0
+            right: 0
+            bottom: 0
+            height: 22px
+            line-height: 22px
+            text-align: center
+            color: $darkTextColor
+            ellipsis()
+</style>
